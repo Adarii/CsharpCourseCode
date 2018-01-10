2732485d029aee1519ad6d2f47e8e56656463048
Adarrii <riad-aljannah@hotmail.com>
2018-01-10 21:26:05 +0000

    DataTypes FloatPoint Type
    This is a program to explain the float Point Type  and the differences among them

---
+++ Float Point/Float Point/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Float_Point
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float fMin = float.MinValue;
+            float fMax = float.MaxValue;
+            Console.WriteLine("Min Value = {0} , Max Vlaue = {1}", fMin, fMax);
+
+            float flVar = 1F / 3;
+            double dVar = 1D / 3;
+            decimal dcVar = 1M / 3;
+            Console.WriteLine("float value = {0}\ndouble value = {1}\ndecimal value = {2}\n", flVar, dVar, dcVar);
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
--- Strings/Strings/Program.cs
+++ Strings/Strings/Program.cs
@@ -11,37 +11,37 @@ namespace Strings
         static void Main(string[] args)
         {
 
+            // Strings :
 
-
-            string mySentence = "Please go to a new  \n  line?";
+            //string mySentence = "Please go to a new  \n  line?";
 
             //string mySentence = "My  \"pretty wife\";
 
             //string mySentence = string.Format("{0}!", "i live in Morocco");  
-              
+
             //string mySentence = string.Format("{0:C}", 240.25);  
-                   
+
             //string mySentence = string.Format("{0:N}", 1234567);
 
             //string mySentence = string.Format("{0:P}", .575);            
 
 
             //------------------------------------
-            /*
-              
-               string mySentence = "";
-               for (int i = 0; i < 50 ; i++)                                                                                        
-                 {                                                                                      
-                    // mySentence = mySentence+ "--" +i.ToString();
-                        mySentence += "--" + i.ToString(); 
-                  } 
-                                                                                                        
-              */
+
+
+            string mySentence = "";
+            for (int i = 0; i < 50; i++)
+            {
+                // mySentence = mySentence+ "--" +i.ToString();
+                mySentence += "- " + i.ToString();
+            }
+
+
 
 
             //------------------------------------
 
-                       
+
 
 
             Console.WriteLine(mySentence);
